13fe19e21405decd590c3249e65574c0267f7e0b
cqpythagoras <magicianhy@126.com>
2020-12-26 14:32:51 +0800

    代码库2020

--- hibernate/src/main/java/org/py/web2020/hibernate/Builder.java
+++ hibernate/src/main/java/org/py/web2020/hibernate/Builder.java
@@ -1,7 +1,7 @@
 package org.py.web2020.hibernate;
 
 public class Builder<T> {
-    public Builder(T obj) {
-
+    public Builder(T target) {
+        target.getClass().getFields();
     }
 }
---
+++ hibernate/src/main/java/org/py/web2020/hibernate/Rel.java
@@ -0,0 +1,32 @@
+package org.py.web2020.hibernate;
+
+import javax.tools.*;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class Rel {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, IOException {
+        String className = "Hello";
+        String methodName = "sayHello";
+        String source = "public class Hello {public Hello() {System.out.println(\"这是一个代码生成的类 默认构造函数\");} public String sayHello() {return \"这是一个自动生成的类！\";} " +
+                "@Override public String toString() {return \"这是一个自动生成的类！\";}}";
+        SimpleJavaFileObject javaFileObject = new JavaSourceFromString(className, source);
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager(new DiagnosticCollector<>(), null, null);
+        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, null,
+                Arrays.asList("-d", "hibernate/target/classes/"), null, Arrays.asList(javaFileObject));
+        if (task.call()) {
+            Object obj = Class.forName(className).getDeclaredConstructor().newInstance();
+            Method method = obj.getClass().getMethod("toString");
+            System.out.println(method.invoke(obj));
+            Method sayHiMethod = obj.getClass().getMethod(methodName);
+            System.out.println(sayHiMethod.invoke(obj));
+        } else {
+            System.out.println("类生成失败");
+        }
+        fileManager.close();
+    }
+}
